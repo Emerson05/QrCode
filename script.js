52b7491e4516f42f42a66a79189e56aee852ac81
Emerson05 <emersonjuniormt@gmail.com>
2023-12-20 22:42:06 -0300

    corringindo

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-let imgBOx = document.getElementById("imgBox")
+let imgBox = document.getElementById("imgBox")
 let qrImage = document.getElementById("qrImage")
 let qrText = document.getElementById("qrText")
 
 function generateQR(){
 
-    if(qrText.value.lenght > 0){
-        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example"
+    if(qrText.value.length > 0){
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
         + qrText.value
-        imgBOx.classList.add("show-img")
+        imgBox.classList.add("show-img")
     }else{
-        qrText.classList.add('erro');
+        qrText.classList.add('error');
         setTimeout(()=>{
-            qrText.classList.remove('erro');
+            qrText.classList.remove('error');
         },1000);
     }
   
